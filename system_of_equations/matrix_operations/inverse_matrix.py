2284c26fd151d401738a5811793f5700312e5bb9
Марина Низова <maryna.nyzova.04@gmail.com>
2023-11-02 17:26:03 +0200

    -- matrix_2_change_with_docstrings

--- system_of_equations/matrix_operations/inverse_matrix.py
+++ system_of_equations/matrix_operations/inverse_matrix.py
@@ -1,16 +1,58 @@
-import numpy as np  # You can use numpy for matrix operations
+import numpy as np
 import sys
 from matrix import Matrix
 
 class MatrixInversion:
+    """
+    Abstract base class for matrix inversion methods.
+
+    Attributes:
+    - matrix: The matrix to be inverted.
+
+    Methods:
+    - inverse(self): Computes the inverse of the matrix. Subclasses must implement this method.
+    """
     def __init__(self, matrix):
+        """
+        Initialize the MatrixInversion instance.
+
+        Parameters:
+        - matrix: The matrix to be inverted.
+        """
         self.matrix = matrix
 
     def inverse(self):
+        """
+        Compute the inverse of the matrix.
+
+        Raises:
+        NotImplementedError: This method must be implemented by child classes.
+        """
         raise NotImplementedError("Inverse method must be implemented by child classes")
 
 class GaussMatrixInversion(MatrixInversion):
+    """
+    Matrix inversion using the Gauss elimination method.
+
+    Attributes:
+    - matrix: The matrix to be inverted.
+
+    Methods:
+    - inverse(self): Computes the inverse of the matrix using the Gauss elimination method.
+
+    Raises:
+    ValueError: If the matrix is singular and cannot be inverted.
+    """
     def inverse(self):
+        """
+        Compute the inverse of the matrix using the Gauss elimination method.
+
+        Returns:
+        A Matrix instance representing the inverted matrix.
+
+        Raises:
+        ValueError: If the matrix is singular and cannot be inverted.
+        """
         # Implement matrix inversion using Gauss method
         if np.linalg.matrix_rank(self.matrix.data) != self.matrix.rows:
             raise ValueError("Matrix is singular, cannot be inverted using Gauss method")
@@ -38,29 +80,46 @@ class GaussMatrixInversion(MatrixInversion):
         return Matrix(n, n, inverted_data.tolist())
 
 class AlgebraicMatrixInversion(MatrixInversion):
+    """
+    Matrix inversion using the matrix of algebraic additions (adjugate matrix) method.
+
+    Attributes:
+    - matrix: The matrix to be inverted.
+
+    Methods:
+    - inverse(self): Computes the inverse of the matrix using the adjugate matrix method.
+
+    Raises:
+    ValueError: If the matrix is singular and cannot be inverted.
+    """
     def inverse(self):
-        # Implement matrix inversion using the matrix of algebraic additions (adjugate matrix)
+        """
+        Compute the inverse of the matrix using the adjugate matrix method.
+
+        Returns:
+        A Matrix instance representing the inverted matrix.
+
+        Raises:
+        ValueError: If the matrix is singular and cannot be inverted.
+        """
         if np.linalg.matrix_rank(self.matrix.data) != self.matrix.rows:
             raise ValueError("Matrix is singular, cannot be inverted using the adjugate matrix method")
         
-        # Your implementation of the adjugate matrix method for inversion here
-        adjugate_matrix = np.linalg.inv(self.matrix.data)
-        inverted_matrix = np.linalg.det(self.matrix.data) * adjugate_matrix
-        return Matrix(self.matrix.rows, self.matrix.columns, inverted_matrix.tolist())
+        n = self.matrix.rows
+        adjugate_matrix = np.zeros((n, n))
 
+        for i in range(n):
+            for j in range(n):
+                minor = np.delete(np.delete(self.matrix.data, i, axis=0), j, axis=1)
+                cofactor = (-1) ** (i + j) * np.linalg.det(minor)
+                adjugate_matrix[j, i] = cofactor
 
-matrix_data = [
-    [1, 1, 3],
-    [1, 3, -3],
-    [-2, -4, -4],
-]
+        determinant = np.linalg.det(self.matrix.data)
+        
+        if determinant == 0:
+            raise ValueError("The matrix is singular and cannot be inverted.")
+        
+        inverted_matrix = adjugate_matrix / determinant
+        inverted_matrix_rounded = np.round(inverted_matrix, 2)
 
-matrix_instance = Matrix(3, 3, matrix_data)
-
-# Using GaussMatrixInversion
-gauss_inverter = GaussMatrixInversion(matrix_instance)
-gauss_inverse_matrix = gauss_inverter.inverse()
-
-print("Inverse Matrix using Gauss method:")
-print(gauss_inverse_matrix)
-
+        return Matrix(self.matrix.rows, self.matrix.columns, inverted_matrix_rounded.tolist())
--- system_of_equations/matrix_operations/matrix.py
+++ system_of_equations/matrix_operations/matrix.py
@@ -1,4 +1,26 @@
 class Matrix:
+    """
+    A class representing a mathematical matrix.
+
+    Attributes:
+    - rows: The number of rows in the matrix.
+    - columns: The number of columns in the matrix.
+    - data: A two-dimensional list storing the matrix elements.
+
+    Methods:
+    - __init__(self, rows, columns, data=None): Initializes a matrix with the specified dimensions.
+    - __str__(self): Returns a string representation of the matrix.
+    - __add__(self, other): Performs matrix addition with another matrix of the same dimensions.
+    - __sub__(self, other): Performs matrix subtraction with another matrix of the same dimensions.
+    - __mul__(self, other): Performs matrix multiplication with another matrix that satisfies the multiplication rule.
+    - transpose(self): Returns the transpose of the matrix, swapping rows and columns.
+
+    Note:
+    - Matrix dimensions must match for addition and subtraction.
+    - For multiplication, the number of columns in the first matrix must match the number of rows in the second matrix.
+    - Other matrix operations can be added as needed.
+    """
+
     def __init__(self, rows, columns, data=None):
         self.rows = rows
         self.columns = columns
@@ -12,27 +34,78 @@ class Matrix:
             self.data = data
 
     def __str__(self):
+        """
+        Returns a string representation of the matrix.
+
+        Example:
+        1  2  3
+        4  5  6
+        7  8  9
+        """
         return "\n".join(["\t".join(map(str, row)) for row in self.data])
 
     def __add__(self, other):
+        """
+        Performs matrix addition with another matrix of the same dimensions.
+
+        Parameters:
+        - other: Another matrix to add to this matrix.
+
+        Returns:
+        A new matrix representing the sum of the two matrices.
+
+        Raises:
+        ValueError if matrix dimensions do not match.
+        """
         if self.rows != other.rows or self.columns != other.columns:
             raise ValueError("Matrix dimensions must match for addition")
         result = [[self.data[i][j] + other.data[i][j] for j in range(self.columns)] for i in range(self.rows)]
         return Matrix(self.rows, self.columns, result)
 
     def __sub__(self, other):
+        """
+        Performs matrix subtraction with another matrix of the same dimensions.
+
+        Parameters:
+        - other: Another matrix to subtract from this matrix.
+
+        Returns:
+        A new matrix representing the result of the subtraction.
+
+        Raises:
+        ValueError if matrix dimensions do not match.
+        """
+
         if self.rows != other.rows or self.columns != other.columns:
             raise ValueError("Matrix dimensions must match for subtraction")
         result = [[self.data[i][j] - other.data[i][j] for j in range(self.columns)] for i in range(self.rows)]
         return Matrix(self.rows, self.columns, result)
 
     def __mul__(self, other):
+        """
+        Performs matrix multiplication with another matrix that satisfies the multiplication rule.
+
+        Parameters:
+        - other: Another matrix to multiply with this matrix.
+
+        Returns:
+        A new matrix representing the result of matrix multiplication.
+
+        Raises:
+        ValueError if matrix dimensions do not match for multiplication.
+        """
         if self.columns != other.rows:
             raise ValueError("Number of columns in the first matrix must be equal to the number of rows in the second matrix for multiplication")
         result = [[sum(self.data[i][k] * other.data[k][j] for k in range(self.columns)) for j in range(other.columns)] for i in range(self.rows)]
         return Matrix(self.rows, other.columns, result)
 
     def transpose(self):
+        """
+        Returns the transpose of the matrix, swapping rows and columns.
+
+        Returns:
+        A new matrix representing the transpose of the original matrix.
+        """
         result = [[self.data[j][i] for j in range(self.rows)] for i in range(self.columns)]
         return Matrix(self.columns, self.rows, result)
 
