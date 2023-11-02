784b270d2dae9678781e6e4c67a9393be9f5c543
Марина Низова <maryna.nyzova.04@gmail.com>
2023-11-02 17:54:58 +0200

    -- adding docstrings

--- system_of_equations/decorator/decorators.py
+++ system_of_equations/decorator/decorators.py
@@ -1,9 +1,19 @@
 import numpy as np
 from system_of_equations.errors.exceptions import NonSquareMatrixError, SingularMatrixError, InvalidMatrixStructureError
 
-
-
 def check_square(func):
+    """
+    A decorator that checks if the input matrix is square before executing the decorated function.
+
+    Parameters:
+    - func (callable): The function to be decorated, which operates on a square matrix.
+
+    Raises:
+    - NonSquareMatrixError: If the input matrix is not square (i.e., the number of rows is not equal to the number of columns).
+
+    Returns:
+    - callable: A decorated function that operates on a square matrix.
+    """
     def wrapper(matrix, *args, **kwargs):
         if matrix.shape[0] != matrix.shape[1]:
             raise NonSquareMatrixError(matrix)
@@ -11,6 +21,18 @@ def check_square(func):
     return wrapper
 
 def check_singular(func):
+    """
+    A decorator that checks if the input matrix is singular (non-invertible) before executing the decorated function.
+
+    Parameters:
+    - func (callable): The function to be decorated, which operates on a non-singular matrix.
+
+    Raises:
+    - SingularMatrixError: If the input matrix is singular (i.e., its determinant is zero).
+
+    Returns:
+    - callable: A decorated function that operates on a non-singular matrix.
+    """
     def wrapper(matrix, *args, **kwargs):
         if np.linalg.det(matrix) == 0:
             raise SingularMatrixError()
@@ -18,6 +40,18 @@ def check_singular(func):
     return wrapper
 
 def check_tridiagonal(func):
+    """
+    A decorator that checks if the input matrix has a tridiagonal structure before executing the decorated function.
+
+    Parameters:
+    - func (callable): The function to be decorated, which operates on a tridiagonal matrix.
+
+    Raises:
+    - InvalidMatrixStructureError: If the input matrix does not have a tridiagonal structure.
+
+    Returns:
+    - callable: A decorated function that operates on a tridiagonal matrix.
+    """
     def wrapper(matrix, *args, **kwargs):
         off_diagonal_sum = sum(abs(matrix[i, j]) for i in range(matrix.shape[0]) for j in range(matrix.shape[1]) if abs(i-j) > 1)
         if off_diagonal_sum != 0:
--- system_of_equations/errors/exceptions.py
+++ system_of_equations/errors/exceptions.py
@@ -5,26 +5,44 @@ class LinearEquationError(Exception):
 class NonSquareMatrixError(LinearEquationError):
     """Error for cases when the matrix is not square."""
     def __init__(self, matrix):
+        """Initialize NonSquareMatrixError with a non-square matrix.
+
+        Args:
+            matrix (ndarray): The non-square matrix that caused the error.
+        """
         self.matrix = matrix
         super().__init__(f"Matrix of shape {matrix.shape} is not square!")
 
 class SingularMatrixError(LinearEquationError):
     """Error for cases when the matrix is singular (undefined or degenerate)."""
     def __init__(self):
+        """Initialize SingularMatrixError."""
         super().__init__("Matrix is singular or degenerate!")
 
 class IterationDivergenceError(LinearEquationError):
     """Error indicating divergence of the iteration method."""
     def __init__(self, max_iterations):
+        """Initialize IterationDivergenceError with the maximum number of iterations.
+
+        Args:
+            max_iterations (int): The maximum number of iterations attempted before divergence.
+        """
         super().__init__(f"Iteration method is divergent after {max_iterations} iterations!")
 
 class InvalidMatrixStructureError(LinearEquationError):
     """Error for cases when the matrix does not match the expected structure."""
     def __init__(self, matrix, expected_type):
+        """Initialize InvalidMatrixStructureError with a matrix and its expected structure.
+
+        Args:
+            matrix (ndarray): The matrix that does not match the expected structure.
+            expected_type (str): The expected structure description.
+        """
         self.matrix = matrix
-        super().__init__(f"Matrix does not match the expected structure: {expected_type}")
+        super().__init(f"Matrix does not match the expected structure: {expected_type}")
 
 class ConvergenceError(LinearEquationError):
     """Error indicating convergence issues of the method."""
     def __init__(self):
+        """Initialize ConvergenceError."""
         super().__init__("Convergence issues with the method!")
