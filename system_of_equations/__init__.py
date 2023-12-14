4ab2aa225cfa0324a008889ee1f638b4099cb8bc
Марина Низова <maryna.nyzova.04@gmail.com>
2023-12-14 16:23:09 +0200

    --changing note

--- system_of_equations/__init__.py
+++ system_of_equations/__init__.py
@@ -3,7 +3,6 @@ from .equation_solvers.gauss_method import *
 from .equation_solvers.matrix_method import *
 from .equation_solvers.simple_iterations_method import *
 from .equation_solvers.three_diagonal_matrix_method import *
-from .equation_solvers.linear_equation_solver import *
 
 from .matrix_operations.inverse_matrix import *
 from .matrix_operations.matrix import *
