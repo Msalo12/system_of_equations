c80a6c6dad5d3b1866d7fa0a071eb827dd44166f
Msalo12 <msalo1209@gmail.com>
2023-12-14 16:28:03 +0200

    Gauss-Diagonal

--- test_solver_methods.py
+++ test_solver_methods.py
@@ -8,20 +8,21 @@ from system_of_equations.equation_solvers.three_diagonal_matrix_method import Tr
 print("Testing GaussJordanMethod...")
 matrix = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]]
 gj_method = GaussJordanMethod(matrix)
-print("Solution:", gj_method.solve_linear_system())
+print("Solution:",  [round(x, 2) for x in gj_method.solve_linear_system()])
 
 # Test for GaussMethod
+
 print("\nTesting GaussMethod...")
 matrix = [[2, 1, -1, 8], [-3, -1, 2, -11], [-2, 1, 2, -3]]
 gm_method = GaussMethod(matrix)
-print("Solution:", gm_method.solve_linear_system())
+print("Solution:",  [round(x, 2) for x in gm_method.solve_linear_system()])
 
 # Test for MatrixMethod
 print("\nTesting MatrixMethod...")
 coefficient_matrix = [[4, 12, -16], [12, 37, -43], [-16, -43, 98]]
 right_hand_side = [1, 2, 3]
 mm_method = MatrixMethod(coefficient_matrix, right_hand_side)
-print("Solution:", mm_method.solve_linear_system())
+print("Solution:",  [round(x, 2) for x in mm_method.solve_linear_system()])
 
 # Test for SimpleIterationMethod
 print("\nTesting SimpleIterationMethod...")
@@ -30,7 +31,7 @@ initial_guess = [1, 1, 1]
 tolerance = 0.01
 max_iterations = 100
 si_method = SimpleIterationMethod(matrix, initial_guess, tolerance, max_iterations)
-print("Solution:", si_method.solve_linear_system())
+print("Solution:",  [round(x, 2) for x in si_method.solve_linear_system()])
 
 # Test for TridiagonalMatrixMethod
 print("\nTesting TridiagonalMatrixMethod...")
@@ -41,4 +42,4 @@ c = [1, 1]  # Above main diagonal (2 elements)
 d = [7, 8, 5]  # Right-hand side (3 elements)
  # Free terms
 td_method = TridiagonalMatrixMethod(a, b, c, d)
-print("Solution:", td_method.solve_linear_system())
+print("Solution:",  [round(x, 2) for x in td_method.solve_linear_system()])
